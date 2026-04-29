26a0ae8c80d284049e70f18c0464b544761f7620
pesqodirqulov-creator <pesqodirqulov@gmail.com>
2026-04-29 17:17:51 +0500

    Delete utils directory

--- utils/helper.py
+++
@@ -1,7 +0,0 @@
-import re
-
-
-async def sanitize_query(query: str) -> str:
-    """Qidiruv so'rovini Telegram tugmalari uchun xavfsiz ko'rinishga keltiradi."""
-    sanitized_query = re.sub(r"\s+", " ", re.sub(r"[^\w\s-]", " ", query, flags=re.UNICODE)).strip()
-    return sanitized_query[:120]
